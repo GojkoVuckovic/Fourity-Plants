565b76d6eeaf8eed993d40c771c7ced4406a72b5
VuckovicGojko <vuckovic.gojko1@gmail.com>
2025-07-04 13:36:35 +0200

    [feat]:reworked plant record service

--- src/service/plant_record.ts
+++ src/service/plant_record.ts
@@ -6,17 +6,12 @@ import {
   DynamoDBDocumentClient,
 } from "@aws-sdk/lib-dynamodb";
 import { createRequestSuccess, RequestResult } from "../requests";
-import {
-  UpdatePlantRecordRequest,
-  PlantRecord,
-  GetPlantRecordListRequest,
-  PlantRecordDatabase,
-} from "../types";
+import { UpdatePlantRecordRequest, GetPlantRecordListRequest } from "../types";
 import { z } from "zod";
 
 export const PlantRecordDataSchema = z.object({
-  plant_uuid: z.string().uuid(),
-  employee_name: z.string().min(1),
+  plantUuid: z.string().uuid(),
+  employeeName: z.string().min(1),
   isWater: z.boolean(),
   isSun: z.boolean(),
   date: z.string().datetime("Date must be a valid ISO 8601 string"),
@@ -25,7 +20,7 @@ export const PlantRecordDataSchema = z.object({
 });
 
 export const PlantRecordSchema = BaseItemSchema.extend({
-  type: z.literal("PLANT_RECORD"),
+  type: z.literal("plantRecord"),
   data: PlantRecordDataSchema,
 });
 
@@ -33,13 +28,13 @@ export const PlantRecordDtoSchema = PlantRecordSchema.transform(
   (plantRecordEntry) => {
     const { SK, data } = plantRecordEntry;
     return {
-      SK,
+      uuid: SK,
       ...data,
     };
   },
 );
 
-export const plantRecordArraySchema = z.array(PlantRecordDtoSchema);
+export const PlantRecordArraySchema = z.array(PlantRecordDtoSchema);
 
 export type CreatePlantRecordDTO = z.infer<typeof PlantRecordDataSchema>;
 export type PlantRecordDatabase = z.infer<typeof PlantRecordSchema>;
@@ -50,73 +45,83 @@ export const plantRecordService = (db: DynamoDBDocumentClient) => {
     async updatePlantRecord(
       req: UpdatePlantRecordRequest,
     ): Promise<RequestResult<"updatePlantRecord", PlantRecord>> {
-      const get_plant_record_command = async () =>
+      const getPlantRecordCommand = async () =>
         await db.send(
           new GetCommand({
             TableName: TABLE_NAME,
             Key: {
-              PK: `PLANT_RECORD#${req.payload.uuid}`,
-              uuid: req.payload.uuid,
+              PK: `plantRecord#${req.payload.uuid}`,
+              SK: req.payload.uuid,
             },
           }),
         );
-      const get_plant_record_result = await processRequest(
-        get_plant_record_command,
+      const getPlantRecordResult = await processRequest(
+        getPlantRecordCommand,
         "updatePlantRecord",
       );
-      if (!get_plant_record_result.success) {
-        return get_plant_record_result;
-      } else {
-        const plant_record_parse = parseData(
-          get_plant_record_result,
-          "updatePlantRecord",
-          PlantRecordSchema,
-        );
-        if (plant_record_parse.success) {
-          const plant_record = plant_record_parse.data;
-          plant_record.data.resolved = true;
-          plant_record.data.additionalInfo = req.payload.additionalInfo;
-          const plant_record_database: PlantRecordDatabase = {
-            PK: `PLANT_RECORD#${plant_record.uuid}`,
-            uuid: plant_record.uuid,
-            type: "PLANT_RECORD",
-            resolved: plant_record.data.resolved,
-            additionalInfo: plant_record.data.additionalInfo,
-            plant_uuid: plant_record.data.plant_uuid,
-            employee_name: plant_record.data.employee_name,
-            isWater: plant_record.data.isWater,
-            isSun: plant_record.data.isSun,
-            date: plant_record.data.date,
-          };
-          const update_plant_record_command = async () =>
-            await db.send(
-              new PutCommand({
-                TableName: TABLE_NAME,
-                Item: plant_record_database,
-              }),
-            );
-          const update_plant_record_result = await processRequest(
-            update_plant_record_command,
-            "updatePlantRecord",
-          );
-          if (update_plant_record_result.success) {
-            return createRequestSuccess("updatePlantRecord")(
-              plant_record,
-              200,
-              "updated successfully",
-            );
-          } else {
-            return update_plant_record_result;
-          }
-        } else {
-          return plant_record_parse;
-        }
+      if (!getPlantRecordResult.success) {
+        return getPlantRecordResult;
       }
+      const plantRecordParse = parseData(
+        getPlantRecordResult.data,
+        "updatePlantRecord",
+        PlantRecordSchema,
+      );
+      if (!plantRecordParse.success) {
+        return plantRecordParse;
+      }
+      const plantRecord = plantRecordParse.data;
+      plantRecord.data.resolved = true;
+      plantRecord.data.additionalInfo = req.payload.additionalInfo;
+      const plantRecordDatabase: PlantRecordDatabase = {
+        PK: plantRecord.PK,
+        SK: plantRecord.SK,
+        type: "plantRecord",
+        GSI: plantRecord.SK,
+        GSI2: "",
+        data: {
+          resolved: plantRecord.data.resolved,
+          additionalInfo: plantRecord.data.additionalInfo,
+          plantUuid: plantRecord.data.plantUuid,
+          employeeName: plantRecord.data.employeeName,
+          isWater: plantRecord.data.isWater,
+          isSun: plantRecord.data.isSun,
+          date: plantRecord.data.date,
+        },
+      };
+      const updatePlantRecordCommand = async () =>
+        await db.send(
+          new PutCommand({
+            TableName: TABLE_NAME,
+            Item: plantRecordDatabase,
+          }),
+        );
+      const updatePlantRecordResult = await processRequest(
+        updatePlantRecordCommand,
+        "updatePlantRecord",
+      );
+      if (!updatePlantRecordResult.success) {
+        return updatePlantRecordResult;
+      }
+      return createRequestSuccess("updatePlantRecord")(
+        {
+          uuid: plantRecord.SK,
+          resolved: plantRecord.data.resolved,
+          additionalInfo: plantRecord.data.additionalInfo,
+          plantUuid: plantRecord.data.plantUuid,
+          employeeName: plantRecord.data.employeeName,
+          isWater: plantRecord.data.isWater,
+          isSun: plantRecord.data.isSun,
+          date: plantRecord.data.date,
+        },
+        200,
+        "updated successfully",
+      );
     },
     async getPlantRecordList(
       req: GetPlantRecordListRequest,
     ): Promise<RequestResult<"getPlantRecordList", PlantRecord[]>> {
-      const get_plant_record_list_command = async () => {
+      const getPlantRecordListCommand = async () => {
         const { Items } = await db.send(
           new QueryCommand({
             TableName: TABLE_NAME,
@@ -126,27 +131,24 @@ export const plantRecordService = (db: DynamoDBDocumentClient) => {
               "#typeAttr": "type",
             },
             ExpressionAttributeValues: {
-              ":typeValue": { S: "PLANT_RECORD" },
+              ":typeValue": "plantRecord",
             },
           }),
         );
         return Items;
       };
-      const get_plant_record_list_result = await processRequest(
-        get_plant_record_list_command,
+      const getPlantRecordListResult = await processRequest(
+        getPlantRecordListCommand,
         "getPlantRecordList",
       );
-      if (get_plant_record_list_result.success) {
-        const parse_data = [get_plant_record_list_result.data];
-        const parse_result = parseData(
-          parseData,
-          "getPlantRecordList",
-          plantRecordArraySchema,
-        );
-        return parse_result;
-      } else {
-        return get_plant_record_list_result;
-      }
+      if (!getPlantRecordListResult.success) return getPlantRecordListResult;
+      const parsedData = getPlantRecordListResult.data;
+      const parseResult = parseData(
+        parsedData,
+        "getPlantRecordList",
+        PlantRecordArraySchema,
+      );
+      return parseResult;
     },
   };
 };
